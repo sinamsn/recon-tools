2fdbb20ac159fef4d79abddc455404597bb77c2b
sinamsn <104708279+sinamsn@users.noreply.github.com>
2023-05-20 11:52:24 +0330

    Add files via upload

---
+++ go-tools.sh
@@ -0,0 +1,58 @@
+go install -v github.com/tomnomnom/assetfinder
+go install -v github.com/tomnomnom/assetfinder@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
+go install -v github.com/projectdiscovery/cdncheck/cmd/cdncheck@latest
+go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install github.com/projectdiscovery/alterx/cmd/alterx@latest
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/proxify/cmd/proxify@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install -v github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
+go install -v github.com/projectdiscovery/notify/cmd/notify@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/tomnomnom/httprobe@latest
+go install github.com/tomnomnom/meg@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/tomnomnom/fff@latest
+go install github.com/ffuf/ffuf/v2@latest
+go install github.com/tomnomnom/qsreplace@latest
+go install github.com/tomnomnom/meg@latest
+go install -v github.com/tomnomnom/anew@latest
+go install -v github.com/tomnomnom/dnsgrep@latest
+go install -v github.com/tomnomnom/concurl@latest
+go install -v  github.com/tomnomnom/hacks/filter-resolved@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/bp0lr/gauplus@latest
+go install -v  github.com/Emoe/kxss@latest
+go install github.com/KathanP19/Gxss@latest
+go install -v github.com/tomnomnom/gf@latest\n
+go install github.com/pwnesia/dnstake/cmd/dnstake@latest
+go install github.com/d3mondev/puredns/v2@latest
+go install -v github.com/sensepost/gowitness@latest
+go install github.com/trickest/dsieve@latest
+go install github.com/j3ssie/cdnstrip@latest
+go install -v  github.com/tomnomnom/burl@latest
+go install github.com/projectdiscovery/tlsx/cmd/tlsx@latest
+go install -v github.com/iangcarroll/cookiemonster@latest
+go install -v github.com/m4dm0e/dirdar@latest
+go install github.com/j3ssie/metabigor@latest
+go install github.com/hakluke/hakip2host@latest
+go install github.com/projectdiscovery/asnmap/cmd/asnmap@latest
+go install -v github.com/LukaSikic/subzy@latest
+go install -v  github.com/haccer/subjack@latest
+go install github.com/gwen001/gitlab-subdomains@latest
+go install github.com/gwen001/github-subdomains@latest
+go install -v github.com/musana/fuzzuli@latest
+go install github.com/edoardottt/csprecon/cmd/csprecon@latest
+go install -v github.com/six2dez/ipcdn@latest
+go install github.com/utkusen/socialhunter@latest
+go install github.com/tomnomnom/unfurl@latest
+go install github.com/dhn/udon@latest
+go install github.com/xhzeem/paramix@latest
+GO111MODULE=on go install -v github.com/lc/subjs@latest
+GO111MODULE=on go install github.com/jaeles-project/gospider@latest
