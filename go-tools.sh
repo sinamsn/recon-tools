d7dda6e2f008d831051d4940027b06c089691d9b
sinamsn <104708279+sinamsn@users.noreply.github.com>
2023-06-05 20:58:08 +0330

    Update go-tools.sh

--- go-tools.sh
+++ go-tools.sh
@@ -5,6 +5,7 @@ go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install github.com/subfinder/goaltdns@latest
 go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
+go install github.com/Josue87/gotator@latest
 go install -v github.com/projectdiscovery/cdncheck/cmd/cdncheck@latest
 go install github.com/edoardottt/favirecon/cmd/favirecon@latest
 go install github.com/dhn/udon@latest
