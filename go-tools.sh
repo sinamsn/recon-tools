71cf302670727b7d89189e4abc40f1b5b6412215
sinamsn <104708279+sinamsn@users.noreply.github.com>
2024-03-07 14:30:41 +0330

    Update go-tools.sh

--- go-tools.sh
+++ go-tools.sh
@@ -1,7 +1,6 @@
 go install -v github.com/tomnomnom/assetfinder@latest
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install github.com/subfinder/goaltdns@latest
 go install github.com/j3ssie/str-replace@latest
 go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
