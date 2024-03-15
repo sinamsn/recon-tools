3886f945a274f9c221507323711381ea4d9cfc7f
sinamsn <104708279+sinamsn@users.noreply.github.com>
2024-03-15 21:00:22 +0330

    Create first_commands.sh

---
+++ first_commands.sh
@@ -0,0 +1,14 @@
+pip install dateutil
+pip install requirements.txt
+pip install scapy
+sudo apt-get --assume-yes install git make gcc
+apt install gobuster -y
+pip install tqdm
+snap install amass
+apt install nodejs -y
+apt install cmdtest -y
+pip3 install dirsearch
+apt insall nikto -y
+sudo apt install zsh -y
+apt install cargo -y
+pip install selenium
